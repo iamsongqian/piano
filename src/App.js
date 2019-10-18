71fa73c0d827b169aba5053834d8854a70a76f5a
songqian <iamsongqian@qq.com>
2019-10-18 21:15:46 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import "./App.css"
+import axios from "axios"
+export default class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			inputValue: "",
+			list: [1],
+		}
+	}
+	change = () => {
+		const { inputValue, list } = this.state
+		this.setState({
+			list: [...list, inputValue],
+		})
+	}
+	handle = e => {
+		this.setState({
+			inputValue: e.target.value,
+		})
+	}
+	decrease = index => {
+		const { list } = this.state
+		list.splice(index, 1)
+		this.setState({
+			list,
+		})
+	}
+	componentDidMount() {
+		axios
+			.get("https://www.easy-mock.com/mock/5da9aed3f4375c1918a3edf6/example/query")
+			.then(res => console.log(res))
+			.catch(err => console.log(err))
+			.finally(console.log(11))
+	}
+	render() {
+		return (
+			<div className="app">
+				<input value={this.state.inputValue} onChange={this.handle} />
+				<button onClick={this.change}>添加</button>
+				{this.state.list.map((item, index) => {
+					return (
+						<ul className="item">
+							<li dangerouslySetInnerHTML={{ __html: item }}></li>
+							<button onClick={() => this.decrease(index)}>x</button>
+						</ul>
+					)
+				})}
+			</div>
+		)
+	}
 }
-
-export default App;
